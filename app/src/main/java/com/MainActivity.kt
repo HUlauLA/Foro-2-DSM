c550f740a18eb6af84b22ea2c594a359f9add21b
PGMercii <90542437+PGMercii@users.noreply.github.com>
2026-05-10 13:15:01 -0600

    MainActivity.kt

--- app/src/main/java/com/MainActivity.kt
+++ app/src/main/java/com/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
 import com.controldegastos.app.ui.PantallaHistorialGastos
 import com.controldegastos.app.ui.PantallaIngresoGastos
 import com.controldegastos.app.ui.theme.ControlDeGastosTheme
@@ -15,21 +14,39 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
 
         setContent {
 
             ControlDeGastosTheme {
 
-                var pantallaActual by remember { mutableStateOf("ingreso") }
+                // Control de pantallas
+                var pantallaActual by remember {
+                    mutableStateOf("ingreso")
+                }
 
-                Scaffold { padding ->
+                Scaffold { paddingValues ->
 
                     when (pantallaActual) {
 
-                        "ingreso" -> PantallaIngresoGastos()
+                        // Pantalla ingreso de gastos
+                        "ingreso" -> PantallaIngresoGastos(
 
-                        "historial" -> PantallaHistorialGastos()
+                            irHistorial = {
+                                pantallaActual = "historial"
+                            }
+
+                        )
+
+                        // Pantalla historial
+                        "historial" -> PantallaHistorialGastos(
+
+                            volver = {
+                                pantallaActual = "ingreso"
+                            }
+
+                        )
                     }
                 }
             }
