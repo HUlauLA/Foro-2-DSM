f9785c06bae59e062985b607ad4430f472ba8fd5
PGMercii <90542437+PGMercii@users.noreply.github.com>
2026-05-10 15:39:37 -0600

    MainActivity.kt

--- app/src/main/java/com/MainActivity.kt
+++ app/src/main/java/com/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.tugastoapp
+package com.controldegastos.app
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,20 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.navigation.compose.rememberNavController
-import com.example.tugastoapp.ui.navigation.AppNavigation
-import com.example.tugastoapp.ui.theme.TuGastoAppTheme
+import com.controldegastos.app.ui.navigation.AppNavigation
+import com.controldegastos.app.ui.theme.ControlDeGastosTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TuGastoAppTheme {
-
-                Surface(color = MaterialTheme.colorScheme.background) {
+            ControlDeGastosTheme {
+                Surface(
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     val navController = rememberNavController()
                     AppNavigation(navController)
                 }
-
             }
         }
     }
