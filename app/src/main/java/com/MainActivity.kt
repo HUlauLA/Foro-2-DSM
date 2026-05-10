6146273772e05dbbdbd3dbdf5a176c661c8e05d8
PGMercii <90542437+PGMercii@users.noreply.github.com>
2026-05-10 15:23:32 -0600

    MainActivity.kt

--- app/src/main/java/com/MainActivity.kt
+++ app/src/main/java/com/MainActivity.kt
@@ -1,38 +1,25 @@
-class MainActivity : ComponentActivity() {
+package com.example.tugastoapp
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.navigation.compose.rememberNavController
+import com.example.tugastoapp.ui.navigation.AppNavigation
+import com.example.tugastoapp.ui.theme.TuGastoAppTheme
+
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
-        val auth = FirebaseAuth.getInstance()
-
         setContent {
-            ControlDeGastosTheme {
+            TuGastoAppTheme {
 
-                var pantallaActual by remember {
-                    mutableStateOf(
-                        if (auth.currentUser == null) "login" else "ingreso"
-                    )
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    val navController = rememberNavController()
+                    AppNavigation(navController)
                 }
 
-                Scaffold {
-                    when (pantallaActual) {
-
-                        "login" -> PantallaLogin(
-                            onLoginSuccess = {
-                                pantallaActual = "ingreso"
-                            }
-                        )
-
-                        "ingreso" -> PantallaIngresoGastos(
-                            irHistorial = { pantallaActual = "historial" }
-                        )
-
-                        "historial" -> PantallaHistorialGastos(
-                            volver = { pantallaActual = "ingreso" }
-                        )
-                    }
-                }
             }
         }
     }
