b42e1458cae68ef6973ea0b6b45d153c4f5d26d0
PGMercii <90542437+PGMercii@users.noreply.github.com>
2026-05-10 15:09:18 -0600

    MainActivity.kt

--- app/src/main/java/com/MainActivity.kt
+++ app/src/main/java/com/MainActivity.kt
@@ -1,51 +1,35 @@
-package com.controldegastos.app
-
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.*
-import com.controldegastos.app.ui.PantallaHistorialGastos
-import com.controldegastos.app.ui.PantallaIngresoGastos
-import com.controldegastos.app.ui.theme.ControlDeGastosTheme
-
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-
         enableEdgeToEdge()
 
-        setContent {
+        val auth = FirebaseAuth.getInstance()
 
+        setContent {
             ControlDeGastosTheme {
 
                 var pantallaActual by remember {
-                    mutableStateOf("ingreso")
+                    mutableStateOf(
+                        if (auth.currentUser == null) "login" else "ingreso"
+                    )
                 }
 
                 Scaffold {
-
                     when (pantallaActual) {
 
-                        // Pantalla ingreso
-                        "ingreso" -> PantallaIngresoGastos(
-
-                            irHistorial = {
-                                pantallaActual = "historial"
-                            }
-
-                        )
-
-                        // Pantalla historial
-                        "historial" -> PantallaHistorialGastos(
-
-                            volver = {
+                        "login" -> PantallaLogin(
+                            onLoginSuccess = {
                                 pantallaActual = "ingreso"
                             }
+                        )
 
+                        "ingreso" -> PantallaIngresoGastos(
+                            irHistorial = { pantallaActual = "historial" }
+                        )
+
+                        "historial" -> PantallaHistorialGastos(
+                            volver = { pantallaActual = "ingreso" }
                         )
                     }
                 }
