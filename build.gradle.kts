4c3bb04d97cc82b7e992ca13a442ca4a4241f49c
PGMercii <90542437+PGMercii@users.noreply.github.com>
2026-05-10 17:20:12 -0600

    build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android") // 
-    id("com.google.gms.google-services")
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.compose) apply false
 }
