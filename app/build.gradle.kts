cb05acc6a0d35f828231de92cb69d707dc656ce2
PGMercii <90542437+PGMercii@users.noreply.github.com>
2026-05-10 16:43:01 -0600

    build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,10 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
 
+implementation platform('com.google.firebase:firebase-bom:32.7.0')
 
+implementation 'com.google.firebase:firebase-auth'
+implementation 'com.google.firebase:firebase-firestore'
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
