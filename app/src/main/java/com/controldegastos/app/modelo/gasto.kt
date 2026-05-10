db157ed107364baf04538cc675e542f256b94140
PGMercii <90542437+PGMercii@users.noreply.github.com>
2026-05-10 12:06:18 -0600

    gasto.kt

--- app/src/main/java/com/controldegastos/app/modelo/gasto.kt
+++ app/src/main/java/com/controldegastos/app/modelo/gasto.kt
@@ -1,1 +1,9 @@
+package com.controldegastos.app.model
 
+data class Gasto(
+    val nombre: String = "",
+    val monto: Double = 0.0,
+    val fecha: String = "",
+    val categoria: String = "",
+    val userId: String = ""
+)
