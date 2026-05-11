b6dcd93c688dccff06c4e430136d62f6125ed7a4
PGMercii <90542437+PGMercii@users.noreply.github.com>
2026-05-10 19:55:19 -0600

    build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,10 +34,5 @@ dependencies {
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
-    implementation("androidx.activity:activity-compose:1.9.0")
-
-    implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.ui:ui-tooling-preview")
 }
